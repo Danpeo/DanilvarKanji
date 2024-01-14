ce11f603e3f23f387b7db694e0414c8b04290308
Danpeo <danilvar4@gmail.com>
2024-01-14 16:53:19 +0400

    Remove unused smart enum

--- DanilvarKanji.Client/obj/Debug/net7.0/ru-RU/DanilvarKanji.Client.resources.cs
+++ DanilvarKanji.Client/obj/Debug/net7.0/ru-RU/DanilvarKanji.Client.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DanilvarKanji.Client")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+452c7c8e14a96af0e908e7058b39250330fbdd89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de1a7128a36a8bcb1a4cbeb772c280fa31fdabf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DanilvarKanji.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DanilvarKanji.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
